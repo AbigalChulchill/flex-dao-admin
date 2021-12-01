5517cdfbca5d0f801df388d992baa81b07a4f508
Tang Wei <wei.tang@coinflex.com>
2021-12-01 17:29:54 +0800

    - css centre - set metamask conn as App state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,26 @@
+import { getConn } from './conn'
+import { useEffect, useState } from "react";
+
 function App() {
+
+  const [conn, setConn] = useState();
+  
+  useEffect( () => {
+    async function fetchData() {
+      try {
+        const _conn = await getConn();
+        setConn(_conn);
+      } catch (err) {
+        if (err.data && err.data.message) {
+          window.alert(err.data.message);
+        } else {
+          window.alert(err);
+        }
+      } 
+    }
+    fetchData();
+  }, [conn]);
+
   return (
     <div className="container">
       <h1>Hello FLEX-DAO</h1>
---
+++ src/conn.js
@@ -0,0 +1,37 @@
+import { ethers, Contract } from 'ethers';
+import * as flex from './contracts/FLEXCoin.json'
+import * as veFlex from './contracts/veFLEX.json'
+import * as dailyPayout from './contracts/DailyPayout.json'
+import * as distributor from './contracts/Distributor.json'
+import * as config from './config.json'
+
+export const getConn = () => {
+  return new Promise((resolve, reject) => {
+    window.addEventListener('load', async () => {
+      if (window && window.ethereum) {
+        await window.ethereum.enable();
+        const proveder = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = proveder.getSigner();
+
+        if (window.ethereum.networkVersion !== '10000') {
+          return reject('metamask is not in smartbch mainnet');
+        } else {
+          return resolve(signer);
+        }
+      }
+    });
+  });
+}
+
+export const getFlex = ( conn ) => {
+  return new Contract(config.contracts_addr.flex, flex.abi, conn)
+}
+export const getVeFlex = ( conn ) => {
+  return new Contract(config.contracts_addr.veFLEX, veFlex.abi, conn)
+}
+export const getPayout = ( conn ) => {
+  return new Contract(config.contracts_addr.daily_payout, dailyPayout.abi, conn)
+}
+export const getDistributor = ( conn ) => {
+  return new Contract(config.contracts_addr.daily_mini_distributor, distributor.abi, conn)
+}

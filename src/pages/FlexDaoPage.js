9c6906a04fb83717a54d8532ed5b9f95dac2a32e
Tang Wei <wei.tang@coinflex.com>
2022-01-13 15:07:42 +0800

    redirect to flex-dao-prod by default

--- src/pages/FlexDaoPage.js
+++ src/pages/FlexDaoPage.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { TopNav } from '../components/layout/TopNav'
 import { FlexDaoPPPage } from '../pages/FlexDaoPPPage';
 // import { FlexDaoStg1Page } from '../pages/FlexDaoStg1Page';
@@ -12,6 +12,9 @@ export const FlexDaoPage = () => {
       <TopNav category='FlexDaoPage'></TopNav>
       <div className='subContent'>
         <Switch>
+          <Route path='/flex-dao' exact>
+            <Redirect to='/flex-dao/prod' />
+          </Route>
           <Route path='/flex-dao/pp'>
             <FlexDaoPPPage />
           </Route>
